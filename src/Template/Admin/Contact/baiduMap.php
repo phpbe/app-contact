ddba3133e34ff54b7393fcd6cad942c6f255c138
liu12 <i@liu12.com>
2022-05-08 11:18:09 +0800

    百茺地图 接口升级

--- src/Template/Admin/Contact/baiduMap.php
+++ src/Template/Admin/Contact/baiduMap.php
@@ -1,43 +1,44 @@
 <be-html>
 <!DOCTYPE html>
-<html lang="zh">
+<html lang="zh-CN">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <style type="text/css">
         html{height:100%;font-size: 14px;}
         body{height:100%;margin:0;padding:0}
         #baidu-map{height:100%}
     </style>
-    <script type="text/javascript" src="https://api.map.baidu.com/api?v=1.5&ak=<?php echo $this->configContact->mapKeyBaidu; ?>"></script>
+    <script src="//api.map.baidu.com/api?type=webgl&v=1.0&ak=<?php echo $this->configContact->mapKeyBaidu; ?>"></script>
 </head>
 <body>
-
 <div id="baidu-map"></div>
 <script type="text/javascript">
-    var oPoint = new BMap.Point(<?php echo $this->configContact->lng; ?>, <?php echo $this->configContact->lat; ?>);
-    var oMap = new BMap.Map("baidu-map");                        // 创建Map实例
 
-    oMap.centerAndZoom(oPoint, 18);
-    oMap.addControl(new BMap.NavigationControl());               // 添加平移缩放控件
-    oMap.addControl(new BMap.ScaleControl());                    // 添加比例尺控件
-    oMap.addControl(new BMap.OverviewMapControl());              //添加缩略地图控件
-    oMap.enableScrollWheelZoom();                            //启用滚轮放大缩小
-    oMap.addControl(new BMap.MapTypeControl());          //添加地图类型控件
+    let map = new BMapGL.Map("baidu-map"); // 创建Map实例
 
-    var oMarker = new BMap.Marker( oPoint );  // 创建标注
-    oMarker.enableDragging();
-    oMap.addOverlay(oMarker);              // 将标注添加到地图中
+    let point = new BMapGL.Point(<?php echo $this->configContact->lng; ?>, <?php echo $this->configContact->lat; ?>);
 
+    map.centerAndZoom(point, 16); // 初始化地图,设置中心点坐标和地图级别
 
-    oMap.addEventListener("click",function(e){
-        oMarker.setPosition(new BMap.Point(e.point.lng, e.point.lat));
-        parent.setLngLat(e.point.lng,e.point.lat);
-    });
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    map.addControl(new BMapGL.ScaleControl()); // 添加比例尺控件
+    map.addControl(new BMapGL.ZoomControl()); // 添加缩放控件
+    map.addControl(new BMapGL.NavigationControl3D()); // 添加3D控件
 
-    oMarker.addEventListener("dragend", function(){
-        var p = oMarker.getPosition();
+    let marker = new BMapGL.Marker(point); // 创建标注
+    marker.enableDragging();
+    map.addOverlay(marker); // 将标注添加到地图中
+
+    map.addEventListener('click', function(e){
+        marker.setPosition(new BMapGL.Point(e.latlng.lng, e.latlng.lat));
+        parent.setLngLat(e.latlng.lng,e.latlng.lat);
+    })
+
+    marker.addEventListener("dragend", function(){
+        let p = marker.getPosition();
         parent.setLngLat(p.lng, p.lat);
     });
 
--- src/Template/Contact/baiduMap.php
+++ src/Template/Contact/baiduMap.php
@@ -1,43 +1,46 @@
 <be-html>
 <!DOCTYPE html>
-<html lang="zh">
+<html lang="zh-CN">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <style type="text/css">
         html{height:100%;font-size: 14px;}
         body{height:100%;margin:0;padding:0}
         #baidu-map{height:100%}
     </style>
-    <script type="text/javascript" src="https://api.map.baidu.com/api?v=1.5&ak=<?php echo $this->configContact->mapKeyBaidu; ?>"></script>
+    <script src="//api.map.baidu.com/api?type=webgl&v=1.0&ak=<?php echo $this->configContact->mapKeyBaidu; ?>"></script>
 </head>
 <body>
 
 <div id="baidu-map"></div>
 <script type="text/javascript">
-    let oPoint = new BMap.Point(<?php echo $this->configContact->lng; ?>, <?php echo $this->configContact->lat; ?>);
-    let oMap = new BMap.Map("baidu-map");                        // 创建Map实例
 
-    oMap.centerAndZoom(oPoint, 18);
-    oMap.addControl(new BMap.NavigationControl());               // 添加平移缩放控件
-    oMap.addControl(new BMap.ScaleControl());                    // 添加比例尺控件
-    oMap.addControl(new BMap.OverviewMapControl());              //添加缩略地图控件
-    oMap.enableScrollWheelZoom();                            //启用滚轮放大缩小
-    oMap.addControl(new BMap.MapTypeControl());          //添加地图类型控件
+    let map = new BMapGL.Map("baidu-map"); // 创建Map实例
 
-    let oMarker = new BMap.Marker( oPoint );  // 创建标注
-    oMap.addOverlay(oMarker);              // 将标注添加到地图中
+    let point = new BMapGL.Point(<?php echo $this->configContact->lng; ?>, <?php echo $this->configContact->lat; ?>);
+
+    map.centerAndZoom(point, 16); // 初始化地图,设置中心点坐标和地图级别
+
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    map.addControl(new BMapGL.ScaleControl()); // 添加比例尺控件
+    map.addControl(new BMapGL.ZoomControl()); // 添加缩放控件
+    map.addControl(new BMapGL.NavigationControl3D()); // 添加3D控件
+
+    let marker = new BMapGL.Marker(point); // 创建标注
+    map.addOverlay(marker); // 将标注添加到地图中
 
     // 创建信息窗口对象
-    let oInfoWindow = new BMap.InfoWindow("<?php echo $this->configContact->markerAddress; ?>", {
+    let infoWindow = new BMapGL.InfoWindow("<?php echo $this->configContact->markerAddress; ?>", {
         width : 200,     // 信息窗口宽度
         height: 60,     // 信息窗口高度
         title : "<strong><?php echo $this->configContact->markerTitle; ?></strong>"  // 信息窗口标题
     });
-    oMap.openInfoWindow(oInfoWindow, oPoint); //开启信息窗口
+    map.openInfoWindow(infoWindow, point); //开启信息窗口
 
-    oMarker.addEventListener("click", function(){this.openInfoWindow(oInfoWindow);});
+    marker.addEventListener("click", function(){this.openInfoWindow(infoWindow);});
 
 </script>
 </body>
